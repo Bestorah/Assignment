acdc289434ae4bc30b9413cec3e21bf82861cc3b
Bestorah <163886148+Bestorah@users.noreply.github.com>
2024-03-20 03:05:20 -0700

    Add files via upload

---
+++ CLASS.js/test.js
@@ -0,0 +1,70 @@
+let String1= 'Persea';
+let String2= 'Americana';
+
+let concatenatedStr = `${String1} ${String2}`
+
+console.log(concatenatedStr);
+
+//replace
+let replacedStr = String2.replace("me","za")
+console.log(replacedStr);
+
+//split
+let splitArray = concatenatedStr.split(" ")
+console.log(splitArray);
+
+//substring
+let substring = String1.slice(2,5)
+console.log(substring);
+
+//repeat
+let repeatedStr = String1.repeat(3)
+console.log(repeatedStr);
+
+//Lastindexof
+let lastIndex = String2.lastIndexOf("na")
+console.log(lastIndex);
+
+//cocatenation
+let finalConcatenation = String1.concat(' ',String2)
+console.log(finalConcatenation);
+
+//Numbers
+let num1 = 7.555560021;
+let num2 = 5331;
+
+//toFixed
+let roundedNum1 = num1.toFixed(7.555560021)
+consol.log(roundedNum1);
+
+//parseInt
+let parsedNum1 = num1.parseInt(7.555560021)
+console.log(parsedNum1);
+
+//toString
+let stringNum2 = num2.toString(5331)
+console.log(stringNum2);
+
+//square method
+let squareRootNum1 = Math.squareRootNum1(7.555560021)
+console.log(squareRootNum1);
+
+//power method
+let powerNum2 = Math.powerNum2(5331,3)
+console.log(powerNum2);
+
+ //ceil method
+ let ceiledNum1 =  Math.ceiledNum1(7.555560021)
+ console.log(ceiledNum1);
+ 
+ //floor method
+ let floorNum1 = Math.floorNum1(7.555560021)
+ console.log(floorNum1);
+
+  //Abs method
+  let absNum1 = Math.absNum1(7.555560021)
+  console.log(absNum1);
+
+
+
+
